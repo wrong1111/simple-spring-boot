95a6bd4aa6950835fed448af9ce5e816e7dfa22b
wyong <wyong@bab8edee-72ee-46bd-b9b3-b7cbb2ae0fd4>
2017-09-11 11:39:54 +0000

    增加了加载指定properties文件。。配置 https

---
+++ templateProject/simple-spring-boot/src/main/java/com/example/ConfigSettings.java
@@ -0,0 +1,53 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "config")
+@PropertySource("classpath:config.properties")
+public class ConfigSettings {
+
+	
+	private Integer serverport;
+	private String  protocol;
+	private String scheme;
+	private Boolean secure;
+	private Integer directport;
+	
+	
+	 
+	public Integer getServerport() {
+		return serverport;
+	}
+	public void setServerport(Integer serverport) {
+		this.serverport = serverport;
+	}
+	public String getProtocol() {
+		return protocol;
+	}
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
+	public String getScheme() {
+		return scheme;
+	}
+	public void setScheme(String scheme) {
+		this.scheme = scheme;
+	}
+	public Boolean getSecure() {
+		return secure;
+	}
+	public void setSecure(Boolean secure) {
+		this.secure = secure;
+	}
+	public Integer getDirectport() {
+		return directport;
+	}
+	public void setDirectport(Integer directport) {
+		this.directport = directport;
+	}
+	
+}
