37d1dca1309c8675e5258263be7213c34218feb9
wyong <wyong@bab8edee-72ee-46bd-b9b3-b7cbb2ae0fd4>
2017-09-11 13:25:56 +0000

    未测试过的。

---
+++ src/main/java/com/example/TrasactionAppConfig.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author wrong1111
+ * 主动开启声明式事务
+ */
+@Configuration
+@EnableTransactionManagement
+public class TrasactionAppConfig {
+
+}
